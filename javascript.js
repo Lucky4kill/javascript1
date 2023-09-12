a5a29e8a81bd598dacd67ed960d552c39f6a8f92
Lucky4kill <132388099+Lucky4kill@users.noreply.github.com>
2023-09-12 12:10:20 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+alert("la cuaneta ha llegado");
+var platoprincipal = parseInt(prompt("costo del plato principal"))
+var postre  = parseInt(prompt("costo del postre"))
+var entrada = parseInt(prompt("costo de la entrada"))
+var bebida = parseInt(prompt("costo de la bebida"))
+alert("presione F12 para ver la consola")
+
+var suma = platoprincipal+postre+entrada+bebida
+var igv = suma*0.18
+var total = suma+igv
+console.log("deveras pagar "+total)
